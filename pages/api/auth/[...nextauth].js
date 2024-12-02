74473396d015b072f1700d826371d01a6086763a
venysssssssssss <viniciussport2004@gmail.com>
2024-12-02 15:19:39 -0300

    Routes of products working and base design of admin's page r up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,13 +3,12 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-})
+});
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
-import {useSession, signIn, signOut} from 'next-auth/react'
-
+import Layout from '@/components/Layout';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
-  const {data: session} = useSession()
-  if (!session) {
-    return (
-      <div className={'bg-blue-900 w-scren h-screen flex items-center'}>
-       <div className="text-center w-full">
-   
-         <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg"> Login with Google</button>
-       
-       </div>
-      </div> 
-     )
-  }
-
-  return (
-    <div> Logged in  {session.user.email}</div>
-  )
- 
+  const {data: session} = useSession();
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.name}</b>
+      </h2>
+      <div className='flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden'>
+        <img src={session?.user?.image} alt="" className='w-6 h-6'></img>
+        <span className='px-2'>
+          {session?.user.name}
+        </span>
+      </div>
+    </div>
+  </Layout>;
 }
